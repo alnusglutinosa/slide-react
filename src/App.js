103f2e0b43be11443d945c2f30da155bc08be969
alnusglutinosa <olga.app2@gmail.com>
2018-12-14 14:39:55 +0200

    1st

--- src/App.js
+++ src/App.js
@@ -2,27 +2,41 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+
+import styled from 'styled-components';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="wrapper">
+        <SayHello />
+        <SayFulName name="Olha" surname="She" link="facebook.com" />
+        <SayFulName name="Ira" surname="She" link="facebook.com" />
       </div>
+      
     );
   }
+}
+
+function SayFulName(props) {
+  return (
+      <div>
+      <h3>my name is {props.name}, surname - {props.surname} </h3>
+      <a href={props.link}> Link... </a>
+      </div>
+  )
+}
+
+function SayHello() {
+  return (
+      <div>
+      <h1>Hello World !</h1>
+      <hr/>
+      </div>
+  )
 }
 
 export default App;
